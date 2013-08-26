b9f1bd3df9910cbabf6826cc60eda3d18728f87a
e339156 <e339156@emb142062.embasa.intranet>
2013-08-26 17:03:38 -0300

    Revert "projeto criado"
    This reverts commit 4b51d7f5ebfe0e100bb29871736e216ef68474c3.

--- helloWorldCpp/main.cpp
+++ helloWorldCpp/main.cpp
@@ -1,21 +1,19 @@
-/*
+/* 
  * File:   main.cpp
- * Author: Daniel Amaral e Silva - dan.amaralsilva@gmail.com
+ * Author: e339156
  *
  * Created on 26 de Agosto de 2013, 16:04
  */
 
 #include <cstdlib>
-#include <stdio.h>
 
 using namespace std;
 
 /*
- *
+ * 
  */
 int main(int argc, char** argv) {
 
-    printf("Hello, World!\n"); // plain obvious what it does
     return 0;
 }
 
