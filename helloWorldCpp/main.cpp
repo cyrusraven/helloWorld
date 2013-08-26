4b51d7f5ebfe0e100bb29871736e216ef68474c3
Daniel Amaral e Silva <>
2013-08-26 17:02:20 -0300

    projeto criado

--- helloWorldCpp/main.cpp
+++ helloWorldCpp/main.cpp
@@ -1,19 +1,21 @@
-/* 
+/*
  * File:   main.cpp
- * Author: e339156
+ * Author: Daniel Amaral e Silva - dan.amaralsilva@gmail.com
  *
  * Created on 26 de Agosto de 2013, 16:04
  */
 
 #include <cstdlib>
+#include <stdio.h>
 
 using namespace std;
 
 /*
- * 
+ *
  */
 int main(int argc, char** argv) {
 
+    printf("Hello, World!\n"); // plain obvious what it does
     return 0;
 }
 
