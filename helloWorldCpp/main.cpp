31c1c4575d7a8118a459f66ceea48481e5f2d6a9
e339156 <e339156@emb142062.embasa.intranet>
2013-08-26 16:08:09 -0300

    projeto criado

---
+++ helloWorldCpp/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: e339156
+ *
+ * Created on 26 de Agosto de 2013, 16:04
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
