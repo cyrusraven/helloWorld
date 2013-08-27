d49b4fd9a94f0e81abccbc9bb1ddfda5b69daf2f
Daniel Amaral e Silva <>
2013-08-27 11:51:27 -0300

    "Guessing Game Challenge" Resolvendo exercícios.
    >> exercício ainda entra em loop infinito

---
+++ CppChallenge_2/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * File:   main.cpp
+ * Author: Daniel Silva
+ *
+ * Created on August 27, 2013, 11:07 AM
+ */
+
+/*
+ * The following program will act as a guessing game in which the user has eight
+ * tries to guess a randomly generated number. The program will tell the user
+ * each time whether he guessed high or low:
+ */
+
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+/*
+ * not solved, still going into a infinite loop
+ * puzzle:
+ * http://www.cprogramming.com/complete/guessing.html
+ */
+int main() {
+    int number = rand();
+    int guess = -1;
+    int trycount = 0;
+
+    while (guess != number || trycount < 8) {
+        cout << "Please enter a guess> ";
+        cin >> guess;
+
+        if (guess > number) {
+            cout << "Too low" << endl;
+        } else if (guess < number) {
+            cout << "Too high" << endl;
+        } else if ( guess == number){
+            cout <<" You guessed the number!";
+            return 0;
+        }
+        trycount++;
+    }
+
+    cout << "Sorry, the number was > "<< number;
+
+    return 0;
+}
+
