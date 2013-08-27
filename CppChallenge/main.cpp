2d1aa67aca82656c81e39dd14a0f9e58dbc1c332
Daniel Amaral e Silva <>
2013-08-27 10:29:53 -0300

    "C++ Programming Challenge - Array Manipulation Challenge" Resolvendo exercícios.
    >> exercício resolvido :D

--- CppChallenge/main.cpp
+++ CppChallenge/main.cpp
@@ -1,19 +1,29 @@
-/* 
+/*
  * File:   main.cpp
- * Author: e339156
+ * Author: Daniel Silva
  *
  * Created on August 27, 2013, 9:52 AM
  */
 
-#include <cstdlib>
+/*
+ * Um array que recebe os valores do usuário e depois os lista
+ */
+
+#include <iostream>
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+int main() {
+    int array[8];
+
+    for (int x = 0; x < 8; x++) {
+        cout << "valor "<< x <<"> ";
+        cin >> array[x];
+    }
+    cout << "\n";
+    cout << "resultados:";
+    for (int x = 0; x < 8; x++)
+        cout << "valor "<< x <<"> "<<array[x]<<"\n";
 
     return 0;
 }
-
