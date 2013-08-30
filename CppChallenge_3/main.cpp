10247aff6218cc925c8f26d7d4ec19851976ef21
Daniel Amaral e Silva <>
2013-08-30 17:37:20 -0300

    "Calculator Challenge" Resolvendo exercícios.
    >> pendente

---
+++ CppChallenge_3/main.cpp
@@ -0,0 +1,47 @@
+/*
+ * File:   main.cpp
+ * Author: Daniel Amaral e Silva
+ *
+ * Created on August 30, 2013, 5:17 PM
+ */
+
+/*
+ * The following program should function as a basic calculator;
+ * it should ask the user to input what type of arithmetic operation
+ * he would like, and then ask for the numbers on which the operation
+ * should be performed. The calculator should then give
+ * the output of the operation.
+ *
+ * http://www.cprogramming.com/complete/calc.html
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * funções algébricas
+ */
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+float divide(int x, int y) {
+    return x / y;
+}
+
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+int main() {
+
+    return 0;
+}
+
